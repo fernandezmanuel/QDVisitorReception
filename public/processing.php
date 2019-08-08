9a0b2e26486bc752fce88e75ae74f6ce76b000d9
Refalm <refalm1@gmail.com>
2019-08-08 16:45:58 +0200

    #1 Switched to prepared statements.

--- public/processing.php
+++ public/processing.php
@@ -3,14 +3,17 @@ require_once("../configuration.php");
 
 if((isset($_POST['visitorname'])) && (isset($_POST['visitormail'])) && (isset($_POST['visitororg'])))
 {
-	$visitorname = $dbconnection->real_escape_string($_POST['visitorname']);
-	$visitormail = $dbconnection->real_escape_string($_POST['visitormail']);
-	$visitororg = $dbconnection->real_escape_string($_POST['visitororg']);
+	$visitorname = mysqli_real_escape_string($dbconnection, $_POST['visitorname']);
+	$visitormail = mysqli_real_escape_string($dbconnection, $_POST['visitormail']);
+	$visitororg = mysqli_real_escape_string($dbconnection, $_POST['visitororg']);
 	
-	$insert = "INSERT INTO visitor (visitorname, visitormail, visitororg) VALUES ('".$visitorname."','".$visitormail."','".$visitororg."')";
+	$checkvisitorname = $_POST['visitorname'];
+	$check = mysqli_query($dbconnection,"SELECT * FROM visitor WHERE visitorname = '".$checkvisitorname."'");
 	
-	if ($dbconnection->query($insert) === TRUE)
+	if (($insert = $dbconnection->prepare("INSERT INTO visitor (visitorname, visitormail, visitororg) VALUES ('".$visitorname."','".$visitormail."','".$visitororg."')")) && (mysqli_num_rows($check)==0))
 	{
+		$insert->execute();
+		$insert->close();
 		echo "<meta http-equiv=\"refresh\" content=\"60; URL=./index.html\" /><style>body { background:#9bdb4d; }</style><span style=\"font-size:128px\">😺</span>\n<br /><br /><span style=\"font=family:'Georgia',serif;font-size:48px;\">Your name has been written in the visitor list, $visitorname!</span><br /><br /><a href=\"./index.html\" style=\"text-decoration:none;\"><button style=\"font-size:24px;cursor:pointer;\">⬅️ Back</button></a>";
 	}
 	else
